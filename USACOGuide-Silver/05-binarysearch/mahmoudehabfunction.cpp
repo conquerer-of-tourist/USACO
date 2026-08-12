c9fb644bddc5795e70be18b3b929a8ecf85ef861
Hengsheng Wang <wanghengsheng9@gmail.com>
2026-08-12 10:46:28 -0700

    starting mahmouud yay

---
+++ USACOGuide-Silver/05-binarysearch/mahmoudehabfunction.cpp
@@ -0,0 +1,24 @@
+/*
+
+    August 12, 2026 - Warmups Sessions
+    hangboy
+    Codeforces - E. Mahmoud and Ehab + Function
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m, q;
+
+int main()
+{
+    cin >> n >> m >> q;
+    vector<int> a (n), b (m);
+    for (auto& val : a) {
+        cin >> val;
+    }
+    for (auto& val : b) {
+        cin >> val;
+    }
+}
