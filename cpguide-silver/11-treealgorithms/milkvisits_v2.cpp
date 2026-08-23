311ff65415d9f14deac524cee1d801c2aa04bd4b
Hengsheng Wang <wanghengsheng9@gmail.com>
2026-08-23 10:05:48 -0700

    milk visit finish

--- cpguide-silver/11-treealgorithms/milkvisits_v2.cpp
+++ cpguide-silver/11-treealgorithms/milkvisits_v2.cpp
@@ -16,14 +16,19 @@ using namespace std;
 int n, m;
 string s;
 
-void dfs(ve<ve<int>>& adj, ve<int>& colorings) {
-    //
+void dfs(ve<ve<int>>& adj, ve<int>& colorings, int curLabel, int i, char type) {
+    for (auto& conn : adj[i]) {
+        if (colorings[conn] == -1 && s[conn] == type) {
+            colorings[conn] = curLabel;
+            dfs(adj, colorings, curLabel, conn, type);
+        }
+    }
 }
 
 int main()
 {
-    // freopen("milkvisits.in", "r", stdin);
-    // freopen("milkvisits.out", "w", stdout);
+    freopen("milkvisits.in", "r", stdin);
+    freopen("milkvisits.out", "w", stdout);
     cin >> n >> m >> s;
 
     ve<ve<int>> adj (n);
@@ -40,7 +45,19 @@ int main()
     int curLabel = 1;
     for (int i = 0; i < n; i++) {
         if (colorings[i] == -1) {
-            
+            colorings[i] = curLabel;
+            dfs(adj, colorings, curLabel, i, s[i]);
+            curLabel++;
         }
     }
+    vector<int> answers;
+    while (m--) {
+        int a, b;
+        char c;
+        cin >> a >> b >> c;
+        a--; b--;
+        if (colorings[a] == colorings[b]) answers.push_back(s[a] == c);
+        else answers.push_back(1);
+    }
+    for (auto& ans : answers) cout << ans;
 }
