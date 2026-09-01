81aa6f6df2196ef4108f946fbba2a14dda423987
Hengsheng Wang <wanghengsheng9@gmail.com>
2026-09-01 16:19:44 -0700

    bsis cp

---
+++ BASISCP2627/e.cpp
@@ -0,0 +1,19 @@
+// Where Am I?
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // freopen("whereami.in", "r", stdin);
+    // freopen("whereami.out", "w", stdout);
+
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+    for (int i = 0; i < n; i++) {
+        //
+    }
+}
