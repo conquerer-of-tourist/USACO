10eb491fd101f176ecdd19d23d9c35962f3bef23
Hengsheng Wang <wanghengsheng9@gmail.com>
2026-09-02 16:28:36 -0700

    doing things

---
+++ cpguide-silver/13-bitwiseoperators/lisamartian.cpp
@@ -0,0 +1,24 @@
+/*
+
+    September 2nd, 2026
+    F. Lisa and the Martians (Codeforces Round 888 Division 3)
+    hangboy
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
