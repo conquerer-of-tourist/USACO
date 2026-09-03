22de8a462b6682cd2d7b2b5a71976892db9a226e
Hengsheng Wang <wanghengsheng9@gmail.com>
2026-09-02 19:44:15 -0700

    lisa martian yipee

--- cpguide-silver/13-bitwiseoperators/lisamartian.cpp
+++ cpguide-silver/13-bitwiseoperators/lisamartian.cpp
@@ -9,14 +9,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define pii pair<ll, ll>
+#define ll long long
+
 void solve() {
-    int n, k;
+    ll n, k;
     cin >> n >> k;
+    vector<pii> vals (n);
+    for (ll i = 0; i < n; i++) {
+        cin >> vals[i].first;
+        vals[i].second = i + 1;
+    }
+    sort(vals.begin(), vals.end());
+
+    ll bestCurr = LLONG_MAX;
+    ll bestI = 0, bestJ = 0, bestX = 0;
+    for (ll i = 0; i < n - 1; i++) {
+        ll a1 = vals[i].first;
+        ll a2 = vals[i + 1].first;
+
+        ll curr = a1 ^ a2;
+        if (curr < bestCurr) {
+            bestCurr = curr;
+            bestI = vals[i].second;
+            bestJ = vals[i + 1].second;
+            bestX = (((ll) 1 << k) - 1) ^ (a1);
+        }
+    }
+    cout << bestI << " " << bestJ << " " << bestX << '\n';
+    return;
 }
 
 int main()
 {
-    int t;
+    ll t;
     cin >> t;
     while (t--) {
         solve();
